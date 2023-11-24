aac77d0991d0ce8367e9a7bfc19c011bf5e9ff62
SantiBj <lsantibz@gmail.com>
2023-11-23 19:48:14 -0500

    Creando el modal para confirmar o cancelar la edicion de viaje y empezando con la feature para la edicion del camion de un viaje

--- src/sections/EditTrip/pages/EditTrip.tsx
+++ src/sections/EditTrip/pages/EditTrip.tsx
@@ -1,8 +1,10 @@
 import { Errors } from "../../../components/Errors";
 import { Loading } from "../../../components/Loading";
+import { ModalGeneric } from "../../../components/ModalGeneric";
 import { Title } from "../../../components/Title";
 import { TittleMajor } from "../../../components/TittleMajor";
 import { useModal } from "../../../hooks/useModal";
+import { ContentM } from "../components/ContentM";
 import { Inputs } from "../components/Inputs";
 import { useLogicEdit } from "../hooks/useLogicEdit";
 
@@ -32,6 +34,14 @@ export function EditTrip() {
   }
   return (
     <article className="space-y-[100px]">
+      <ModalGeneric
+        content={<ContentM
+          closeModal={closeModal}
+          newTrip={inputs.inputs}
+          oldTrip={oldTrip!}
+        />}
+        isOpen={modal}
+      />
       <section className="space-y-[30px]">
         <TittleMajor text="Viaje a editar" />
         <Title
